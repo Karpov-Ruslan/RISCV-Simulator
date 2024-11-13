f6edfb21d3314f54d21a70897e771370e488dbd1
ifrfor <karpov.rv@phystech.edu>
2024-11-13 14:30:51 +0300

    HotFix of include cycle

--- main.cpp
+++ main.cpp
@@ -3,20 +3,5 @@
 #include <bitset>
 
 int main() {
-    if (TestGetField()) {
-        std::cerr << "TestGetField is OK\n";
-    }
 
-    //                     f7      rs2   rs1   f3  rd    op  
-    constexpr Uint add = 0b0000000'00010'00100'000'01000'0110011;
-    constexpr Uint sub = 0b0100000'00010'00100'000'01000'0110011;
-
-    //                      imm          rs1   f3  rd    op  
-    constexpr Uint addi = 0b000000000101'00100'000'01000'0010011;
-    constexpr Uint xori = 0b000000001010'00100'100'01000'0010011;
-
-    Decoder::Decode(add);
-    Decoder::Decode(sub);
-    Decoder::Decode(addi);
-    Decoder::Decode(xori);
 }
---
+++ src/instruction.cpp
@@ -0,0 +1,38 @@
+#include "instruction.hpp"
+#include <hart.hpp>
+
+namespace RISCVS::InstructionSet {
+
+using Register = Hart::Register;
+using RegIdx = Hart::RegisterIndex;
+
+void Add(FUNC_SIGNATURE) {
+    RegIdx rd = std::get<RegIdx>(param1);
+    RegIdx rs1 = std::get<RegIdx>(param2);
+    RegIdx rs2 = std::get<RegIdx>(param3);
+    hart[rd] = hart[rs1] + hart[rs2];
+    hart.MovePC(4U);
+}
+
+void Sub(FUNC_SIGNATURE) {
+    RegIdx rd = std::get<RegIdx>(param1);
+    RegIdx rs1 = std::get<RegIdx>(param2);
+    RegIdx rs2 = std::get<RegIdx>(param3);
+    hart[rd] = hart[rs1] - hart[rs2];
+}
+
+void AddI(FUNC_SIGNATURE) {
+    RegIdx rd = std::get<RegIdx>(param1);
+    RegIdx rs1 = std::get<RegIdx>(param2);
+    Immediate imm = std::get<Immediate>(param3);
+    hart[rd] = hart[rs1] + imm;
+}
+
+void SubI(FUNC_SIGNATURE) {
+    RegIdx rd = std::get<RegIdx>(param1);
+    RegIdx rs1 = std::get<RegIdx>(param2);
+    Immediate imm = std::get<Immediate>(param3);
+    hart[rd] = hart[rs1] - imm;
+}
+
+}
